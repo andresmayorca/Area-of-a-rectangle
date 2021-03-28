05a698d7d63b241025206392b4ebe09633785e2d
Andres Mayorca <70079260+mayorcaandres@users.noreply.github.com>
2021-03-27 21:06:17 -0400

    Add files via upload

---
+++ Area of ​​a rectangle.py
@@ -0,0 +1,8 @@
+def area_rectangulo(base, altura):
+    multiplicacion = base * altura
+    print(multiplicacion)
+
+base = int(input('Escriba la base de su rectangulo para sacar el area: '))
+altura = int(input('Escriba la altura de su rectangulo para sacar el area: '))
+
+area_rectangulo(base, altura)
